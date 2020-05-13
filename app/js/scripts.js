e38f9f8fbb314f82dab5433e8476deecf9e2e30e
Tran Minh Nhat <tmnhat1993@gmail.com>
2020-05-13 14:40:42 +0700

    baisc about page css

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,5 +1,6 @@
 import Common from './modules/common';
 import Home from './modules/home';
+import About from './modules/about';
 // import VideoLazyLoad from './modules/lazy-load-video';
 
 // let lazyLoadVideo = new VideoLazyLoad();
@@ -11,5 +12,10 @@ $(document).ready(function () {
     // Behavior for HomePage
     if ($('#hr-home-page').length > 0) {
         let home = new Home();
+    }
+
+    // Behavior for About Page
+    if ($('#hr-about-page').length > 0) {
+        let about = new About();
     }
 });
